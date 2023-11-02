ad38deae8c842b3cad807b75a988d5c5f2e311ac
Haruto Mori <baystarsman@outlook.jp>
2023-11-02 15:37:37 +0900

    create a function to get lat and lon

--- main.py
+++ main.py
@@ -3,16 +3,30 @@ import requests
 from decouple import config
 
 
-API_KEY = config("API_KEY")
-CITY_NAME = 'Tokyo'
+API_KEY = config('API_KEY')
 
-# get latitude and longitude
-coordinates_url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}"
-coordinates = requests.get(coordinates_url).json()
-LAT, LON = coordinates['coord']['lat'], coordinates['coord']['lon']
 
-# main url
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}"
+def get_latitude_and_longitude(city, api_key=API_KEY):
+    coordinates_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    coordinates = requests.get(coordinates_url).json()
+    LAT, LON = coordinates['coord']['lat'], coordinates['coord']['lon']
 
-response = requests.get(url).json()
-print(response)
+    return LAT, LON
+
+# # main url
+# url = f'https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}'
+
+# response = requests.get(url).json()
+# print(response)
+
+
+
+def main():
+    city = 'Tokyo'
+    latitude, longitude = get_latitude_and_longitude(city)
+    print(latitude)
+    print(longitude)
+
+
+if __name__=='__main__':
+    main()
