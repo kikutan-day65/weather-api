f1791edfbbbeebfc06c61bdb69add8358d70ad79
Haruto Mori <baystarsman@outlook.jp>
2023-11-02 15:29:51 +0900

    change the url to get the coordinates

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from decouple import config
 
 
 API_KEY = config("API_KEY")
-
 CITY_NAME = 'Tokyo'
 
 # get latitude and longitude
-coordinates_url = f"http://api.openweathermap.org/geo/1.0/direct?q={CITY_NAME}&appid={API_KEY}"
+coordinates_url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}"
 coordinates = requests.get(coordinates_url).json()
-LAT, LON = coordinates[0]['lat'], coordinates[0]['lon']
+LAT, LON = coordinates['coord']['lat'], coordinates['coord']['lon']
 
 # main url
 url = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}"
