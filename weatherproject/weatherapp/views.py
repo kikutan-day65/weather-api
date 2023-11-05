5e63a7cfaa84c116d0c07a752342451ca96120ef
Haruto Mori <baystarsman@outlook.jp>
2023-11-05 17:04:02 +0900

    render all weather info

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .helper_func import *
+import datetime
 
 
 # Create your views here.
@@ -7,10 +8,18 @@ def index(request):
     city = enter_city() # Specify the city from html later
     lat, lon = get_latitude_and_longitude(city)
     current = get_current_weather(lat, lon)
+    temperature = convert_to_celsius(current['main']['temp'])
+    icon = current['weather'][0]['icon']
+    description = current['weather'][0]['description']
+    day = datetime.date.today()
+    
     
     context = {
         'city': city,
-        'current': current
+        'temp': temperature,
+        'icon': icon,
+        'description': description,
+        'day': day,
     }
 
     return render(request, 'weatherapp/index.html', context)
