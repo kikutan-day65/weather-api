e9769e1dfe7ce1cfcee1fc208acefb93d0983efe
Haruto Mori <baystarsman@outlook.jp>
2023-11-05 16:47:47 +0900

    get weather info in index

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from helper_func import *
+from .helper_func import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'weatherapp/index.html')
+    city = enter_city() # Specify the city from html later
+    lat, lon = get_latitude_and_longitude(city)
+    current = get_current_weather(lat, lon)
+    
+    context = {
+        'city': city,
+        'current': current
+    }
+
+    return render(request, 'weatherapp/index.html', context)
